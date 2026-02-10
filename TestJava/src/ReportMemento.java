71af61aed89834b4711fa08404108d9ad79790d7
Anastasiia <shevchuk.anastasiia@chnu.edu.ua>
2026-02-10 16:31:46 +0200

    added

---
+++ TestJava/src/ReportMemento.java
@@ -0,0 +1,12 @@
+public class ReportMemento {
+    private final String header;
+    private final String content;
+    private final String footer;
+
+    public ReportMemento(String header, String content, String footer) {
+        this.header = header;
+        this.content = content;
+        this.footer = footer;
+    }
+    // ... (геттери)
+}
