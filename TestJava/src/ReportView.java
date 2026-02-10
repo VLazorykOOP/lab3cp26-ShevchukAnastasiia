bd47c653b772a4558eebd5d786c1f98acb3b15cf
Anastasiia <shevchuk.anastasiia@chnu.edu.ua>
2026-02-10 16:37:12 +0200

    .

---
+++ TestJava/src/ReportView.java
@@ -0,0 +1,8 @@
+public abstract class ReportView {
+    protected RenderEngine engine;
+
+    protected ReportView(RenderEngine engine) {
+        this.engine = engine;
+    }
+    public abstract void display(Report report);
+}
